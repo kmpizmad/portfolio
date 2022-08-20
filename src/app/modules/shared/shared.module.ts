663fc50a5d58d45ed8b67570b854bc2f95faab10
kmpizmad <nagy.viktor392@gmail.com>
2022-08-20 10:40:04 +0200

    Added shared module

---
+++ src/app/modules/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DataService } from './services/data.service';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  providers: [DataService],
+})
+export class SharedModule {}
