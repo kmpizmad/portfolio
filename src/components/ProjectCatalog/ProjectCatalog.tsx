0a44df44e6d49721210a166a3ae394e4bec89dab
kmpizmad <nagy.viktor392@gmail.com>
2024-01-05 23:19:33 +0100

    Configured prettier

---
+++ src/components/ProjectCatalog/ProjectCatalog.tsx
@@ -0,0 +1,7 @@
+function ProjectCatalog(props: Props): JSX.Element | null {
+  return null;
+}
+
+export default ProjectCatalog;
+
+interface Props {}
---
+++ src/components/ProjectCatalogItem/ProjectCatalogItem.tsx
@@ -0,0 +1,11 @@
+function ProjectCatalogItem(props: Props): JSX.Element | null {
+  return null;
+}
+
+export default ProjectCatalogItem;
+
+interface Props {
+  imageUrl: string;
+  headline: string;
+  summary: string;
+}
